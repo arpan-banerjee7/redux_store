36eb4c9977c6fd6825145aca08cb0f538112188a
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-11 18:22:36 +0530

    reducer store complete

--- index.js
+++ index.js
@@ -1,87 +1,130 @@
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
-
-/*
-Characteristics of a Pure Function
-1) They always return the same result if the same arguments are passed in.
-2) They depend only on the arguments passed into them.
-3) Never produce any side effects.
-*/
-
 // Reducer function
-function todos (state = [], action) {
-  switch(action.type) {
-    case 'ADD_TODO' :
-      return state.concat([action.todo])
-    case 'REMOVE_TODO' :
-      return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO' :
-      return state.map((todo) => todo.id !== action.id ? todo :
-        Object.assign({}, todo, {complete: !todo.complete})
-      )
-    default :
-      return state
+function todos(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter((todo) => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
   }
 }
 
-function createStore (reducer) {
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state. (getState)
   // 3. Listen to changes on the state. (subscribe)
   // 4. Update the state (dispatch)
 
-  let state
-  let listeners = []
+  let state;
+  let listeners = [];
 
-  const getState = () => state
+  const getState = () => state;
 
   const subscribe = (listener) => {
-    listeners.push(listener)
+    listeners.push(listener);
     return () => {
-      listeners = listeners.filter((l) => l !== listener)
-    }
-  }
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
   const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
 
   return {
     getState,
     subscribe,
     dispatch,
-  }
+  };
 }
 
-const store = createStore(todos)
+const store = createStore(app);
+
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Walk the dog",
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 1,
+    name: "Wash the car",
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 2,
+    name: "Go to the gym",
+    complete: true,
+  },
+});
+
+store.dispatch({
+  type: "REMOVE_TODO",
+  id: 1,
+});
+
+store.dispatch({
+  type: "TOGGLE_TODO",
+  id: 0,
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 0,
+    name: "Learn Redux",
+  },
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 1,
+    name: "Lose 20 pounds",
+  },
+});
+
+store.dispatch({
+  type: "REMOVE_GOAL",
+  id: 0,
+});
